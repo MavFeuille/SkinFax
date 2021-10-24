efe4848660613b4ac3362d2499789c9e5758494b
huongw <whitneyhuong@gmail.com>
2021-10-24 00:12:00 +0000

    test route worked successfully

--- skinfax_api/server.js
+++ skinfax_api/server.js
@@ -7,13 +7,15 @@ const PORT = process.env.PORT || 8080;
 const ENV = process.env.ENV || 'development';
 const express = require('express');
 const app = express();
+app.set('port', PORT);
 
+const testAPIRouter = require('./routes/testAPI');
 
-// app.use('/', )
+app.use('/testAPI', testAPIRouter);
 
 
 app.get('/', (req, res) => {
-  res.send('Hello world');
+  res.redirect(testAPIRouter);
 });
 
 app.listen(PORT, console.log(`Server is listening on port ${PORT}`));
