94a7056a668641b00d12dde34be72f9943e02e99
WH <whitneyhuong+gitkraken@gmail.com>
2021-10-23 23:32:48 -0400

    made changes to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,30 +4,30 @@ import { useState } from 'react';
 
 function App() {
 
-  const [name, setName] = useState('');
-  const [age, setAge] = useState();
+  // const [name, setName] = useState('');
+  // const [age, setAge] = useState();
+  const [value, setValue] = useState([])
 
   const onClick = function () {
     axios.get("/api/test")
       .then((res) => {
-        setName(res.data.name)
-        setAge(res.data.age)
+        // setName(res.data.name)
+        // setAge(res.data.age)
+        setValue(res.data)
+        console.log(res.data)
       })
   }
-  const clear = function () {
-    setAge('')
-    setName('')
-  }
+
+  // const clear = function () {
+  //   setAge('')
+  //   setName('')
+  // }
 
   return (
     <div className="App">
-      <button onClick={onClick}>api</button>
-      <button onClick={clear}>clear</button>
+      <button onClick={onClick}>click me</button>
       <div>
-        {name}
-      </div>
-      <div>
-        {age}
+        {/* {value} */}
       </div>
     </div>
   );
