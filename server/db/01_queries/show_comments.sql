74d43a13f0c401ceb260c2683034eaa43338c9c6
MavFeuille <mavis.feuille@gmail.com>
2021-10-25 19:29:45 -0400

    Add query to show comments

---
+++ server/db/01_queries/show_comments.sql
@@ -0,0 +1,5 @@
+SELECT users.username as username, comments.comment, comments.created FROM comments
+JOIN content_posts ON content_post_id = content_posts.id
+JOIN users ON content_posts.user_id = users.id
+WHERE content_posts.id = 1
+ORDER BY created DESC;
