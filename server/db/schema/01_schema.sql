04166d4cff0dc59bc7c1c94959795b673da33d13
MavFeuille <mavis.feuille@gmail.com>
2021-10-25 15:41:36 -0400

    Update schema

--- server/db/schema/01_schema.sql
+++ server/db/schema/01_schema.sql
@@ -11,18 +11,21 @@ CREATE TABLE users (
   username VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL
-  profile_picture VARBINARY (max) 
+  profile_picture_url VARCHAR(255) NOT NULL,
+  is_active BOOLEAN DEFAULT TRUE
 );
 
 CREATE TABLE content_posts (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  created timestamp default current_timestamp,
+  image_video_url VARCHAR(255) NOT NULL,
   description VARCHAR(1024) NOT NULL
+  created timestamp default current_timestamp,
 );
 
 CREATE TABLE favourites (
   id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   content_post_id INTEGER REFERENCES content_posts(id) ON DELETE CASCADE,
 );
 
@@ -33,7 +36,8 @@ CREATE TABLE followers (
 
 CREATE TABLE direct_messages (
   id SERIAL PRIMARY KEY NOT NULL,
-  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  to_user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  from_user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   message VARCHAR(1024) NOT NULL
   created timestamp default current_timestamp
 );
@@ -48,7 +52,6 @@ CREATE TABLE comments (
 CREATE TABLE likes (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  comment_id INTEGER REFERENCES comments(id) ON DELETE CASCADE,
+  content_post_id INTEGER REFERENCES content_posts(id) ON DELETE CASCADE,
+  comment_id INTEGER REFERENCES comments(id) ON DELETE CASCADE
 );
-
---how to add image/video content_posts db
