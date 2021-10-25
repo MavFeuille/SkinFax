581a715e556949f38ac80f528485b9f58e2c4656
MavFeuille <mavis.feuille@gmail.com>
2021-10-25 19:20:29 -0400

    Update query to grab all following posts

---
+++ server/db/01_queries/user_create_new_post.sql
@@ -0,0 +1,3 @@
+INSERT INTO content_posts (id, user_id, image_video_url, description)
+VALUES 
+(4,3, 'url','Princess Peach is testing for a new post');
--- server/db/01_queries/user_follow.sql
+++ server/db/01_queries/user_follow.sql
@@ -1,6 +1,6 @@
 -- Follow a new user
 INSERT INTO followers (id, follower_user_id, user_id)
-VALUES (4, 2, 3);
+VALUES (5, 1, 2);
 
 -- Listing all the followers username
 SELECT username FROM users
@@ -15,7 +15,8 @@ WHERE follower_user_id = 3;
 --prints princess peach
 
 -- List all the posts that the user is following
-SELECT content_posts.user_id as PostCreator, image_video_url, description, created FROM content_posts
+SELECT users.username as following_users, image_video_url, description, created FROM content_posts
 JOIN users ON content_posts.user_id = users.id
 JOIN followers ON followers.user_id = users.id 
-WHERE users.id = 1;
+WHERE followers.follower_user_id = 1
+ORDER BY created DESC;
