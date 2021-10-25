d37ff22aca7d60419848c349e837e78adda802e2
Kadia5 <kdabraeux@github.com>
2021-10-25 15:39:24 -0400

    seeds file information updated to newer version

--- server/db/seeds/01_seeds.sql
+++ server/db/seeds/01_seeds.sql
@@ -1,54 +1,43 @@
-DROP TABLE IF EXISTS users CASCADE;
-DROP TABLE IF EXISTS content_posts CASCADE;
-DROP TABLE IF EXISTS favourites CASCADE;
-DROP TABLE IF EXISTS followers CASCADE;
-DROP TABLE IF EXISTS direct_messages CASCADE;
-DROP TABLE IF EXISTS comments CASCADE;
-DROP TABLE IF EXISTS likes CASCADE;
+INSERT INTO users (id, username, name, email, password, profile_picture_URL)
+VALUES
+(1, 'mario', 'mario@hotmail.com', '$2a$10$GbpxiC5B2.ab7v9hae.uruaMuI.8LVySWcgwSbPCEUMsyUT8hfJhG', 'https://www.imore.com/sites/imore.com/files/styles/large/public/field/image/2021/03/mario-hero.jpg', TRUE), 
+(2, 'luigi', 'luigi@deviantart.com', '$2a$10$GbpxiC5B2.ab7v9hae.uruaMuI.8LVySWcgwSbPCEUMsyUT8hfJhG', 'https://ssb.wiki.gallery/images/6/67/Luigi_SSBB.jpg', TRUE),
+(3, 'Princess Peach', 'peach@gmail.com', '$2a$10$GbpxiC5B2.ab7v9hae.uruaMuI.8LVySWcgwSbPCEUMsyUT8hfJhG', 'https://pbs.twimg.com/profile_images/1317551648617730048/JY6m4Hfh.jpg', TRUE);
 
-CREATE TABLE users (
-  id SERIAL PRIMARY KEY NOT NULL,
-  username VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL
-  profile_picture VARBINARY (max) 
-);
+INSERT INTO content_posts (id, user_id, description, image_video_URL*)
+VALUES 
+(1,2,'description 1','url'),
+(2,3,'description 2','url'),
+(3,1,'description 3','url');
 
-CREATE TABLE content_posts (
-  id SERIAL PRIMARY KEY NOT NULL,
-  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  created timestamp default current_timestamp,
-  description VARCHAR(1024) NOT NULL
-);
+INSERT INTO favourites (id, user_id content_post_id)
+VALUES 
+(1,1,2), 
+(2,2,3), 
+(3,3,1);
 
-CREATE TABLE favourites (
-  id SERIAL PRIMARY KEY NOT NULL,
-  content_post_id INTEGER REFERENCES content_posts(id) ON DELETE CASCADE,
-);
+INSERT INTO followers(id, user_id)
+VALUES 
+(1, 3),
+(2, 1),
+(3, 2);
 
-CREATE TABLE followers (
-  id SERIAL PRIMARY KEY NOT NULL,
-  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-);
+INSERT INTO direct_messages (to_user_id, from_user_id, message)
+VALUES 
+(1,2, 'This is a test msg'),
+(2,3, 'This is another test msg'),
+(3,1, 'This is the last test msg');
 
-CREATE TABLE direct_messages (
-  id SERIAL PRIMARY KEY NOT NULL,
-  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  message VARCHAR(1024) NOT NULL
-  created timestamp default current_timestamp
-);
+INSERT INTO comments (user_id, content_post_id)
+VALUES 
+(1,3),
+(2,1),
+(3,2);
 
-CREATE TABLE comments (
-  id SERIAL PRIMARY KEY NOT NULL,
-  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  content_post_id INTEGER REFERENCES content_posts(id) ON DELETE CASCADE,
-  created timestamp default current_timestamp
-);
+INSERT INTO likes (user_id, content_post_id, comment_id)
+VALUES 
+(1,3,2),
+(2,1,3),
+(3,2,1);
 
-CREATE TABLE likes (
-  id SERIAL PRIMARY KEY NOT NULL,
-  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  comment_id INTEGER REFERENCES comments(id) ON DELETE CASCADE,
-);
-
---how to add image/video content_posts db
+--inserting content image via 3rd party ting
