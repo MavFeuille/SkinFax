e30bca03e1820a641f7b74aaf92c398b4aff234d
MavFeuille <mavis.feuille@gmail.com>
2021-10-25 21:29:29 -0400

    Update queries

--- server/db/01_queries/user_unfollow.sql
+++ server/db/01_queries/user_unfollow.sql
@@ -1,2 +1,6 @@
 DELETE FROM followers
-WHERE followers.user_id = 3;
+WHERE users.id= _ AND followers.user_id = 3;
+
+-- cloudinary, aws as 3
+-- socket.io
+--- window.setTimeout to check query every 5 sec
