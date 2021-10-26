5563d42108766f43ca1f3f3568b38c1cc1e0146c
Kadia5 <kdabraeux@github.com>
2021-10-26 17:56:40 +0000

    dm route setup

--- server/src/routes/direct_messages.js
+++ server/src/routes/direct_messages.js
@@ -1,52 +1,61 @@
 const express = require('express');
 const router = express.Router();
-
-// function that will contain all the get routes
+// function that will contain all the get routes;
 const routers = function (pool) {
-  router.get('/direct_messages', function (req, res) {
+
+  router.get('/direct_messages', function (req, res, next) {
+
     const queryString = `
     SELECT message, from_user_id, created
     FROM direct_messages
     JOIN users ON users.id = direct_messages.from_user_id
     WHERE users.username = 'mario'
     `;
-    
-      return pool
-      .query(queryString)
-      .then((data) => {
-        res.json(data.rows);
-      })
-      .catch((err) => {
-        console.log('error:', err.message);
-      });
 
-      
+    return pool.query(queryString) 
+      .then((data) => {
+        const directMessage = data.rows[0];
+
+        if (directMessage) {
+          return res.json(directMessage)
+        }
+        return res.status(404).send("not available")
+    
+      })
+      .catch(err => {
+        console.log('error:', err.message);
+        return next (err)
+      })
   });
 
-  router.get('/direct_messages/to', function (req, res) {
-    const queryString = `
-    Select message, to_user_id, created
+  router.get('/direct_message/to', function (req, res, next) {
+
+    const  queryString = 
+    `
+    SELECT message, to_user_id, created
     FROM direct_messages
     JOIN users ON users.id = direct_messages.to_user_id
-    WHERE users.username = 'luigi';
+    WHERE users.username = 'luigi'
     `;
-    
-      return pool
-      .query(queryString)
+
+    return pool.query(queryString) 
       .then((data) => {
-       return res.json(data.rows);
-
+        const dm = data.rows;
+          return res.json(dm);
       })
-      .catch((err) => {
+      .catch(err => {
         console.log('error:', err.message);
-      });
+        return next (err)
+      })
 
-      
   });
-
   //only return router
   return router;
-};
+}
 
 // only export the function
 module.exports = routers;
+
+
+
+
