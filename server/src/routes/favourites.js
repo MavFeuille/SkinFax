78b9cdc49b426927750e5f989c49b7a1c24bf9ff
WH <whitneyhuong+gitkraken@gmail.com>
2021-10-25 22:22:27 -0400

    add post routes for favourites

--- server/src/routes/favourites.js
+++ server/src/routes/favourites.js
@@ -12,7 +12,7 @@ const routers = function (pool) {
       JOIN favourites ON content_post_id = content_posts.id
       JOIN users ON favourites.user_id = users.id
       WHERE users.id = 3
-      ORDER BY created DESC;`
+      ORDER BY created DESC;`;
 
     pool.query(queryString)
       .then((data) => {
@@ -22,6 +22,47 @@ const routers = function (pool) {
         console.log('error:', err.message);
       });
   });
+
+  router.post("/favourites", (req, res) => {
+    const queryString = `INSERT INTO favourites (user_id, content_post_id)
+    VALUES 
+    ($1,$2);`;
+
+    const user_id = req.session.user_id;
+    const content_post_id = req.session.content_post_id;
+
+    const val = [user_id, content_post_id];
+
+    db.query(queryString, val)
+      .then(() => {
+        res.redirect("/");
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  });
+
+  router.post("/delete/:id", (req, res) => {
+
+    const user_id = req.session.user_id;
+    const removeItem = req.params.id;
+
+    const queryString = `
+    DELETE FROM favourites
+    WHERE user_id = 2 AND favourites.id = 4;`;
+
+    const val = [user_id, removeItem]
+
+    db.query(queryString, val)
+      .then(() => {
+        res.redirect("/favourites");
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  })
 
   //only return router
   return router;
