f9ccbe6256203870a44e980d472db2bffb8c00d6
Kadia5 <kdabraeux@github.com>
2021-10-26 23:55:37 +0000

    test 4 dm route

--- server/db/01_queries/messages_between_user.sql
+++ server/db/01_queries/messages_between_user.sql
@@ -11,3 +11,11 @@ JOIN users ON users.id = direct_messages.to_user_id
 WHERE users.username = 'luigi';
 
 -- let gary double check
+
+-- query for message all-in-one
+
+SELECT message, from_user_id, to_user_id, created
+FROM direct_messages
+JOIN users u_1 ON u_1.id = direct_messages.from_user_id
+JOIN users u_2 ON u_2.id = direct_messages.to_user_id
+WHERE u_1.username = 'luigi' AND u_2.username = 'Princess Peach';
