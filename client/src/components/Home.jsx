7baf832b9882f94cff362f23dc2e9b1e87619cf1
MavFeuille <mavis.feuille@gmail.com>
2021-10-27 18:25:10 -0400

    Add useEffect in home.jsx

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,5 +1,22 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+
 export default function Home() {
+  const [value, setValue] = useState([])
+
+  useEffect(() => {  
+    axios.get("/")
+    .then ((res) => {
+       setValue(res.data)
+    }).catch ((err) => {
+       console.log (err)
+   })
+  }, [])
+
   return(
     <h1>Home</h1>
+
+  
   )
 }
