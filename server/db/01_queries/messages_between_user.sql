9dac8d3e2c70bad3e88aa6bd96cd5a43f87177c9
Kadia5 <kdabraeux@github.com>
2021-10-26 20:15:33 -0400

    new query that receiving/sending dms into 1 test 5

--- server/db/01_queries/messages_between_user.sql
+++ server/db/01_queries/messages_between_user.sql
@@ -13,9 +13,31 @@ WHERE users.username = 'luigi';
 -- let gary double check
 
 -- query for message all-in-one
+--from_user_id = sender
+--to_user_id = receiver
 
-SELECT message, from_user_id, to_user_id, created
+SELECT *
 FROM direct_messages
-JOIN users u_1 ON u_1.id = direct_messages.from_user_id
-JOIN users u_2 ON u_2.id = direct_messages.to_user_id
-WHERE u_1.username = 'luigi' AND u_2.username = 'Princess Peach';
+INNER JOIN(
+SELECT MAX(id) as id FROM (
+SELECT MAX(id) as id, from_user_id as contact
+FROM direct_messages
+WHERE to_user_id = 'luigi'
+GROUP BY from_user_id
+
+UNION ALL
+
+SELECT MAX(id) as id, to_user_id as contact
+FROM direct_messages
+WHERE from_user_id = 'Princess Peach'
+GROUP BY to_user_id
+) t GROUP BY contact
+) d
+ON direct_messages.id = d.id
+ORDER BY created DESC;
+
+
+
+-- users u_1 ON u_1.id = direct_messages.from_user_id
+-- JOIN users u_2 ON u_2.id = direct_messages.to_user_id
+-- WHERE u_1.username = 'luigi' AND u_2.username = 'Princess Peach';
