854a933897eb0d72e01412c78bd10eb8bacdcf64
WH <whitneyhuong+gitkraken@gmail.com>
2021-10-30 19:55:21 -0400

    add trash icon for favorites

--- client/src/components/Favourites.jsx
+++ client/src/components/Favourites.jsx
@@ -1,43 +1,38 @@
-import axios from 'axios';
-import { useState, useEffect } from 'react';
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 export default function Favourites() {
-
   const [favourites, setFavourites] = useState([]);
 
   useEffect(() => {
-    axios.get('/api/favourites')
-    .then((res) => {
-      setFavourites(res.data)
-    })
-    .catch((err) => {
-      console.log(err.message)
-    })
-  }, [])
+    axios
+      .get("/api/favourites")
+      .then((res) => {
+        setFavourites(res.data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
 
   const userFavourites = favourites.map((obj) => {
     return (
       <>
-        <div>
-        <span>
-          {obj.username}
-        </span>
-        <br/>
-        <span>
-          {obj.created}
-        </span>
-        </div>
-        <div>
-        <img src={obj.image_video_url} alt="" />
-        <p>{obj.description}</p>
-        </div>
+        <section>
+          <span>{obj.username}</span>
+          <br />
+          <span>{obj.created}</span>
+        </section>
+        <section>
+          <img src={obj.image_video_url} alt="" />
+          <p>{obj.description}</p>
+          <button type="button">
+            <i class="fas fa-trash"></i>
+          </button>
+        </section>
       </>
     );
-  })
+  });
 
-  return(
-    <div>
-      {userFavourites}
-    </div>
-  );
+  return <div>{userFavourites}</div>;
 }
