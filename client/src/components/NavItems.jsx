21dfc2a677c0384f98767189956b132bebca645c
WH <whitneyhuong+gitkraken@gmail.com>
2021-10-30 16:02:32 -0400

    fix header

--- client/src/components/NavItems.jsx
+++ client/src/components/NavItems.jsx
@@ -1,36 +1,58 @@
-import Button from 'react-bootstrap/Button';
-export const HOME_PAGE = 'Home';
-export const PROFILE_PAGE = 'Shop';
-export const FAV_PAGE = 'favourites';
-export const CREATE_POST = 'create_post';
-export const LOGIN = 'login';
-export const REGISTER = 'register';
-export const ROUTER_MESSAGES = 'router_messages';
+import Button from "react-bootstrap/Button";
+export const HOME_PAGE = "Home";
+export const PROFILE_PAGE = "Shop";
+export const FAV_PAGE = "favourites";
+export const CREATE_POST = "create_post";
+export const LOGIN = "login";
+export const REGISTER = "register";
+export const ROUTER_MESSAGES = "router_messages";
 
 export default function NavItems(props) {
   return (
     <ul>
-      <li onClick={() => { props.setPage(HOME_PAGE)}}>
+      <li
+        onClick={() => {
+          props.setPage(HOME_PAGE);
+        }}
+      >
         <i class="fas fa-home" />
       </li>
-      <li onClick={() => {props.setPage(PROFILE_PAGE)}}>
+      <li
+        onClick={() => {
+          props.setPage(PROFILE_PAGE);
+        }}
+      >
         <i class="far fa-user-circle" />
       </li>
-      <li onClick={() => {props.setPage(FAV_PAGE)}}>
+      <li
+        onClick={() => {
+          props.setPage(FAV_PAGE);
+        }}
+      >
         <i class="far fa-bookmark" />
       </li>
-      <li onClick={() => {props.setPage(CREATE_POST)}}>
+      <li
+        onClick={() => {
+          props.setPage(CREATE_POST);
+        }}
+      >
         <i class="far fa-plus-square"></i>
       </li>
-      <li onClick={() => {props.setPage(ROUTER_MESSAGES)}}>
+      <li
+        onClick={() => {
+          props.setPage(ROUTER_MESSAGES);
+        }}
+      >
         <i class="far fa-comment-dots"></i>
       </li>
       <li>{props.user.email}</li>
-
-      <li onClick={() => {props.setPage(ROUTER_MESSAGES)}}>Join Messages</li>
-      <li onClick={() => {props.logout()}}>
-        <Button variant="outline-secondary">Logout</Button>{' '}</li>
-
+      <li
+        onClick={() => {
+          props.logout();
+        }}
+      >
+        <Button variant="outline-secondary">Logout</Button>{" "}
+      </li>
     </ul>
   );
 }
