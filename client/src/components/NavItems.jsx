1af5b52aa6cfb6bcd8970fb47ede93c5cd367876
MavFeuille <mavis.feuille@gmail.com>
2021-10-31 18:03:06 -0400

    Update Navbar to be fixed position

--- client/src/components/NavItems.jsx
+++ client/src/components/NavItems.jsx
@@ -9,50 +9,52 @@ export const ROUTER_MESSAGES = "router_messages";
 
 export default function NavItems(props) {
   return (
-    <ul>
-      <li
-        onClick={() => {
-          props.setPage(HOME_PAGE);
-        }}
-      >
-        <i class="fas fa-home" />
-      </li>
-      <li
-        onClick={() => {
-          props.setPage(PROFILE_PAGE);
-        }}
-      >
-        <i class="far fa-user-circle" />
-      </li>
-      <li
-        onClick={() => {
-          props.setPage(FAV_PAGE);
-        }}
-      >
-        <i class="far fa-bookmark" />
-      </li>
-      <li
-        onClick={() => {
-          props.setPage(CREATE_POST);
-        }}
-      >
-        <i class="far fa-plus-square"></i>
-      </li>
-      <li
-        onClick={() => {
-          props.setPage(ROUTER_MESSAGES);
-        }}
-      >
-        <i class="far fa-comment-dots"></i>
-      </li>
-      <li>{props.user.email}</li>
-      <li
-        onClick={() => {
-          props.logout();
-        }}
-      >
-        <Button variant="outline-secondary">Logout</Button>{" "}
-      </li>
-    </ul>
+    <div className="navbar">
+      <ul>
+        <li
+          onClick={() => {
+            props.setPage(HOME_PAGE);
+          }}
+        >
+          <i class="fas fa-home" />
+        </li>
+        <li
+          onClick={() => {
+            props.setPage(PROFILE_PAGE);
+          }}
+        >
+          <i class="far fa-user-circle" />
+        </li>
+        <li
+          onClick={() => {
+            props.setPage(FAV_PAGE);
+          }}
+        >
+          <i class="far fa-bookmark" />
+        </li>
+        <li
+          onClick={() => {
+            props.setPage(CREATE_POST);
+          }}
+        >
+          <i class="far fa-plus-square"></i>
+        </li>
+        <li
+          onClick={() => {
+            props.setPage(ROUTER_MESSAGES);
+          }}
+        >
+          <i class="far fa-comment-dots"></i>
+        </li>
+        <li>{props.user.email}</li>
+        <li
+          onClick={() => {
+            props.logout();
+          }}
+        >
+          <Button variant="outline-secondary">Logout</Button>{" "}
+        </li>
+      </ul>
+    </div>
   );
 }
