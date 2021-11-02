66dd713d9e6453f2c0d669d0426cfa5fe8cdc4f5
WH <whitneyhuong+gitkraken@gmail.com>
2021-11-02 18:54:45 -0400

    fix query for posts

--- client/src/components/Posts/PostListItem.jsx
+++ client/src/components/Posts/PostListItem.jsx
@@ -6,59 +6,53 @@ export default function PostListItem(props) {
   const {
     followList,
     creatorUserID,
-    user,
     postId,
     profilePictureUrl,
     imageVideoUrl,
     isOwner,
+    user,
     created,
     username,
     description,
     deletePost,
     addFavourite,
   } = props;
-  console.log(
-    "🚀 ~ file: PostListItem.jsx ~ line 8 ~ PostListItem ~ creatorUserID",
-    creatorUserID
-  );
+  console.log(isOwner);
+  console.log(user.id);
+  // Follow new friend
+  const handleFollow = () => {
+    axios
+      .post(`/api/follow/${creatorUserID}`, { userID: props.user.id })
+      .then((res) => {
+        console.log("🚀 ~ file: Explore.jsx ~ line 17 ~ .then ~ res", res);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
 
-   // Follow new friend
-   const handleFollow = () => {
-    console.log("🚀 ~ file: Explore.jsx ~ line 70 ~ Explore ~ user.id", props.user.id)
-   
-    axios.post(`/api/follow/${creatorUserID}`, {userID: props.user.id})
-    .then((res) => {
-      console.log("🚀 ~ file: Explore.jsx ~ line 17 ~ .then ~ res", res)
-      
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
-  }
-
-    return (
-      <section className="post">
-        <div className="info">
-          <div className="user">
-            <div>
-              <img
-                className="user-profile-pic"
-                src={profilePictureUrl}
-                alt="profile image"
-              />
-            </div>
-            <p className="username"> {username}</p>
-            {followList && !followList.includes(creatorUserID) && !isOwner && 
-              <form onSubmit={event=> event.preventDefault()}>
-                <button  onClick={handleFollow }>Follow</button>
-              </form>
-            }
-            {followList && followList.includes(creatorUserID) && !isOwner && 
-              <p>Following</p>
-            }
+  return (
+    <section className="post">
+      <div className="info">
+        <div className="user">
+          <div>
+            <img
+              className="user-profile-pic"
+              src={profilePictureUrl}
+              alt="profile image"
+            />
           </div>
+          <p className="username"> {username}</p>
+          {followList && !followList.includes(creatorUserID) && !isOwner && (
+            <form onSubmit={(event) => event.preventDefault()}>
+              <button onClick={handleFollow}>Follow</button>
+            </form>
+          )}
+          {followList && followList.includes(creatorUserID) && !isOwner && (
+            <p>Following</p>
+          )}
         </div>
-      
+      </div>
 
       <div className="post-content">
         <img className="post-image" src={imageVideoUrl} alt="" />
