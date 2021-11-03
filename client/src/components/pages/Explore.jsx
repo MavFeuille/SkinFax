8b6f8f62a3d89a3bd77f921bcca3d83924ebcba6
WH <whitneyhuong+gitkraken@gmail.com>
2021-11-03 12:24:49 -0400

    fix header and title

--- client/src/components/pages/Explore.jsx
+++ client/src/components/pages/Explore.jsx
@@ -1,41 +1,47 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import PostList from "../Posts/PostList";
-import "./Explore.css"
+import "./Explore.css";
 
 export default function Explore(props) {
-// console.log("🚀 ~ file: Explore.jsx ~ line 8 ~ Explore ~ props", props)
+  // console.log("🚀 ~ file: Explore.jsx ~ line 8 ~ Explore ~ props", props)
   const [posts, setPosts] = useState([]);
   const [followList, setFollowList] = useState([]);
-  
-  
+
   useEffect(() => {
     axios
-    .get(`/api/posts`)
-    .then((res) => {
-      console.log("🚀 ~ file: Explore.jsx ~ line 15 ~ .then ~ res", res)
-      setPosts(res.data);
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
+      .get(`/api/posts`)
+      .then((res) => {
+        console.log("🚀 ~ file: Explore.jsx ~ line 15 ~ .then ~ res", res);
+        setPosts(res.data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
   }, []);
-  
+
   // Get the list of friends that the user is following
   useEffect(() => {
-    axios.get(`/api/follow/${props.user.id}`)
-    .then((res) => {
-      console.log("🚀 ~ file: Explore.jsx ~ line 29 ~ .then ~ res", res);
-      setFollowList(res.data);
-      console.log("🚀 ~ file: Explore.jsx ~ line 31 ~ .then ~ res.data", res.data)
-    })
-    .catch((err) => {
-      console.log("🚀 ~ file: Explore.jsx ~ line 34 ~ findExistingFollowing ~ err", err);
-    })
-  },[]);
+    axios
+      .get(`/api/follow/${props.user.id}`)
+      .then((res) => {
+        console.log("🚀 ~ file: Explore.jsx ~ line 29 ~ .then ~ res", res);
+        setFollowList(res.data);
+        console.log(
+          "🚀 ~ file: Explore.jsx ~ line 31 ~ .then ~ res.data",
+          res.data
+        );
+      })
+      .catch((err) => {
+        console.log(
+          "🚀 ~ file: Explore.jsx ~ line 34 ~ findExistingFollowing ~ err",
+          err
+        );
+      });
+  }, []);
   ////
-  
-  console.log("🚀 ~ file: Explore.jsx ~ line 38 ~ Explore ~ posts", posts)
+
+  console.log("🚀 ~ file: Explore.jsx ~ line 38 ~ Explore ~ posts", posts);
 
   const addFavourite = (id) => {
     // event.preventDefault();
@@ -51,8 +57,8 @@ export default function Explore(props) {
       .catch((err) => {
         console.log(err.message);
       });
-  }
-  
+  };
+
   const deletePost = function (id) {
     console.log("deleting post, post ID: ", id);
     axios
@@ -64,17 +70,15 @@ export default function Explore(props) {
         console.log(err.message);
       });
   };
-  
- 
-  
+
   // Follow new friend
   // const handleFollow = () => {
   //   console.log("🚀 ~ file: Explore.jsx ~ line 70 ~ Explore ~ user.id", props.user.id)
-   
+
   //   axios.post(`/api/follow/${posts.user_id}`, {userID: props.user.id})
   //   .then((res) => {
   //     console.log("🚀 ~ file: Explore.jsx ~ line 57 ~ .then ~ res", res)
-      
+
   //   })
   //   .catch((err) => {
   //     console.log(err.message);
@@ -85,8 +89,16 @@ export default function Explore(props) {
   return (
     <section className="mainContainer">
       <div>
-        <h1 className="title">Explore</h1>
-        {posts.length && <PostList posts={posts} user={props.user} deletePost={deletePost} addFavourite={addFavourite} followList={followList} />}
+        <h1 id="title">Explore</h1>
+        {posts.length && (
+          <PostList
+            posts={posts}
+            user={props.user}
+            deletePost={deletePost}
+            addFavourite={addFavourite}
+            followList={followList}
+          />
+        )}
       </div>
     </section>
   );
